cb150ef0cb4a11539696a2b3cefdb0c517d6b3f3
Alessandro Santospirito <santospirito@fzi.de>
2023-05-13 22:32:58 +0000

    Writing code to generate unique tiles for a given tile

--- src/tiles.cpp
+++ src/tiles.cpp
@@ -15,3 +15,11 @@ std::vector<Matrix> calculate_all_tiles(Matrix tile) {
         tile_flipped_once, tile_rotated_once_flipped, tile_rotated_twice_flipped, tile_rotated_thrice_flipped
     };
 }
+
+std::set<Matrix> calculate_all_unique_tiles(Matrix tile) {
+    std::vector<Matrix> all_tiles = calculate_all_tiles(tile);
+
+    std::set<Matrix> uniqueTiles(all_tiles.begin(), all_tiles.end());
+
+    return uniqueTiles;
+}
